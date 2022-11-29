662f8cb387d60ec2ec779614a48016b00d132885
Francesc Vila <francescvilasubias@gmail.com>
2022-11-29 17:52:06 +0100

    chore: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App: React.FC = () => (
+  <div className="App">
+    <h1 className="font-bold">Repositori CompREd - test</h1>
+  </div>
+);
+
+export default App;
