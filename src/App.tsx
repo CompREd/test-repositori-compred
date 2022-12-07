3798cedc002d354d60e598de9ebdf44ca73767d7
Francesc Vila <francescvilasubias@gmail.com>
2022-12-07 17:55:51 +0100

    feat: filter by level

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import React, { useState } from 'react';
 import { v4 as uuid } from 'uuid';
 
@@ -166,16 +167,16 @@ const resources: Array<Resource> = [
 
 const App: React.FC = () => {
   const [filterDimensions, setFilterDimensions] = useState<Array<string>>([]);
+  const [filterLevels, setFilterLevels] = useState<Array<number>>([]);
 
   const dimensions = resources.map((resource) => resource.dimension).filter(onlyUnique);
 
   return (
     <div className="overflow-scroll w-screen h-screen">
       <div className="grid grid-cols-6 w-full h-full p-2 bg-gray-100">
-        <div className="col-span-1 h-full border-r-gray-400 border-r mx-2">
-          <div className="flex gap-2 flex-col mx-4 h-full justify-center">
+        <div className="col-span-1 h-full border-r-gray-400 border-r mx-2 flex flex-col justify-center gap-12">
+          <div className="flex gap-2 flex-col mx-4 justify-center">
             <span className="font-bold underline text-center">Dimensions</span>
-            <button type="button" className="rounded-full px-2 py-1" onClick={() => setFilterDimensions([])}>Totes</button>
             { dimensions.map((d) => {
               const color = dimensionToColor.get(d);
               return (
@@ -195,10 +196,32 @@ const App: React.FC = () => {
                 </button>
               );
             })}
+            <button type="button" className="rounded-full px-2 py-1 text-white bg-gray-300" onClick={() => setFilterDimensions([])}>Totes</button>
+          </div>
+          <div className="flex gap-2 flex-col mx-4 justify-center items-center">
+            <span className="font-bold underline text-center">Nivells</span>
+            {
+              Array(3).fill(0).map((e, index) => (
+                <button
+                  key={uuid()}
+                  type="button"
+                  className={`bg-gray-300 w-fit pr-4 items-center rounded-full ${filterLevels.includes(index + 1) ? 'fill-yellow-200' : 'fill-white'} flex justify-center`}
+                  onClick={() => {
+                    if (!filterLevels.includes(index + 1)) {
+                      setFilterLevels(filterLevels.concat(index + 1));
+                    } else {
+                      setFilterLevels(filterLevels.filter((currentFilters) => currentFilters !== index + 1));
+                    }
+                  }}
+                >
+                  {[...Array(index + 1)].map(() => <svg className="scale-[.5] mr-[-15px]" key={uuid()} xmlns="http://www.w3.org/2000/svg" height="48" width="48"><path d="M24 44q-1.7 0-2.875-1.175T19.95 39.95h8.1q0 1.7-1.175 2.875T24 44Zm-6.6-7.15q-.65 0-1.075-.425Q15.9 36 15.9 35.35q0-.65.425-1.075.425-.425 1.075-.425h13.2q.65 0 1.075.425.425.425.425 1.075 0 .65-.425 1.075-.425.425-1.075.425Zm-1.25-6.05q-3.3-2.15-5.225-5.375Q9 22.2 9 18.15q0-6.1 4.45-10.55Q17.9 3.15 24 3.15q6.1 0 10.55 4.45Q39 12.05 39 18.15q0 4.05-1.9 7.275-1.9 3.225-5.25 5.375Z" /></svg>)}
+                </button>
+              ))
+            }
           </div>
         </div>
         <div className="col-span-5 overflow-scroll h-full flex flex-col gap-3 rounded-md mx-4">
-          { resources.filter((r) => filterDimensions.length === 0 || filterDimensions.includes(r.dimension)).map((resource) => {
+          { resources.filter((r) => filterLevels.length === 0 || filterLevels.includes(r.level)).filter((r) => filterDimensions.length === 0 || filterDimensions.includes(r.dimension)).map((resource) => {
             const color = dimensionToColor.get(resource.dimension);
             return (
               <div key={uuid()} className="flex flex-col relative p-4 rounded-md bg-white shadow-md">
@@ -207,7 +230,6 @@ const App: React.FC = () => {
                 <a className="hover:text-gray-500 w-fit" href={resource.url} target="_blank" rel="noreferrer"><h1 className="font-bold">{`${resource.type}: ${resource.title}`}</h1></a>
                 <span>{resource.subdimension ? `→ ${resource.subdimension}` : ''}</span>
                 <div className="py-2" />
-                {/* eslint-disable-next-line max-len */}
                 <a aria-label="resource-link" className="hover:fill-gray-500" href={resource.url} target="_blank" rel="noreferrer"><svg className="absolute right-6 top-[28%]" xmlns="http://www.w3.org/2000/svg" height="48" width="48"><path d="M14 34q-4.25 0-7.125-2.875T4 24q0-4.25 2.875-7.125T14 14h7q.65 0 1.075.425.425.425.425 1.075 0 .65-.425 1.075Q21.65 17 21 17h-7q-3 0-5 2t-2 5q0 3 2 5t5 2h7q.65 0 1.075.425.425.425.425 1.075 0 .65-.425 1.075Q21.65 34 21 34Zm3.75-8.5q-.65 0-1.075-.425-.425-.425-.425-1.075 0-.65.425-1.075.425-.425 1.075-.425h12.5q.65 0 1.075.425.425.425.425 1.075 0 .65-.425 1.075-.425.425-1.075.425ZM27 34q-.65 0-1.075-.425-.425-.425-.425-1.075 0-.65.425-1.075Q26.35 31 27 31h7q3 0 5-2t2-5q0-3-2-5t-5-2h-7q-.65 0-1.075-.425-.425-.425-.425-1.075 0-.65.425-1.075Q26.35 14 27 14h7q4.25 0 7.125 2.875T44 24q0 4.25-2.875 7.125T34 34Z" /></svg></a>
                 <div className="flex items-center">
                   <span>Nivell:</span>
